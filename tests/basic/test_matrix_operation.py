a77b73f67493f1e01d1fa303d7391556cfc5c24b
wj-Mcat <1435130236@qq.com>
2022-06-08 21:12:14 +0800

    improve the paddle reshape test code

--- tests/basic/test_matrix_operation.py
+++ tests/basic/test_matrix_operation.py
@@ -1,8 +1,10 @@
 import paddle
 
 
-def test_paddle_replace():
+def test_paddle_reshape():
     a = paddle.randn((2,3, 4))
     b = paddle.reshape_(a, (2,4, 3))
-    
+
     assert id(b) == id(a)
+
+    assert b.shape == [2, 4, 3]
